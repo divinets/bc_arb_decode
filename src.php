bb0c782952ac0c3bde9a6298c078b2960a30cfd1
Divinets <35217731+divinets@users.noreply.github.com>
2023-07-13 17:45:50 +0700

    Update src.php

--- src.php
+++ src.php
@@ -5,7 +5,7 @@ function bc_arb_decode($num, $basestr)
     $dec = '0';
     $num_arr = str_split((string)$num);
     $cnt = strlen($num);
-    for ($i = 0; $i < $cnt; $i++) {
+    for ($i = 0; $i < $cnt; $i++) { 
         $pos = strpos($basestr, $num_arr[$i]);
         if ($pos === false) Throw new Exception(sprintf('Unknown character %s at offset %d', $num_arr[$i], $i));
         $dec = bcadd(bcmul($dec, $base), $pos);
